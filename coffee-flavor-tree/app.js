068a3cc33fe596555b79e5ea0705cdd174ecb533
Dara Kilicoglu <darakilicoglu@gmail.com>
2023-12-23 23:27:55 +0100

    Update Coffee Flavor Tree app. (1.1.0)

--- coffee-flavor-tree/app.js
+++ coffee-flavor-tree/app.js
@@ -8,81 +8,96 @@
  * Author: Dara Kilicoglu
  * Copyright: 2023
  * License: MIT
- * Version: 1.0.0
+ * Version: 1.1.0
  * Maintained by: Dara Kilicoglu
  */
 
 document.addEventListener("DOMContentLoaded", async () => {
-    const rootContainer = document.getElementById('rootContainer');
+    const rootContainer = createFlavorContainer('flavor-container');
+    document.body.appendChild(rootContainer);
 
-    const isColorDark = (hexColor) => {
+    function isColorDark(hexColor) {
         const [r, g, b] = hexColor.match(/\w\w/g).map(hex => parseInt(hex, 16));
         const luminance = 0.2126 * r + 0.7152 * g + 0.0722 * b;
         return luminance < 102;
-    };
+    }
 
-    const humanize = (string) => string.replace(/(?:^|\W)\w/g, match => match.toUpperCase());
+    function humanize(string) {
+        return string.replace(/(?:^|\W)\w/g, match => match.toUpperCase());
+    }
 
-    const toggleFlavorExpansion = (flavorElement) => {
+    function toggleFlavorExpansion(flavorElement) {
         const isExpanded = flavorElement.classList.toggle('expanded');
         flavorElement.parentElement.classList.toggle('active', isExpanded);
-    };
+    }
 
-    const handleFlavorClick = (event) => {
+    function handleFlavorClick(event) {
         event.stopPropagation();
         const flavorElement = event.currentTarget;
-    
-        if (!flavorElement.classList.contains('expandable')) {
-            return;
-        }
-    
         const isCurrentlyExpanded = flavorElement.classList.contains('expanded');
-        Array.from(flavorElement.parentElement.children).forEach(child => 
-            child.classList.remove('expanded'));
-    
+
+        Array.from(flavorElement.parentElement.children).forEach(child => {
+            child.classList.remove('expanded');
+        });
+
         flavorElement.classList.toggle('expanded', !isCurrentlyExpanded);
         flavorElement.parentElement.classList.toggle('active', !isCurrentlyExpanded);
-    };
+    }
 
-    const createFlavorElement = (flavor, index = 0) => {
-        const element = document.createElement('div');
-        element.className = 'flavor';
+    function createFlavorElement(flavor, index = 0, level = 0) {
+        const textColor = isColorDark(flavor.color) ? 'rgba(255, 255, 255, 0.75)' : 'rgba(0, 0, 0, 0.8)';
+        const element = createElementWithClass(`flavor level-${level}`);
         element.style.backgroundColor = flavor.color;
         element.style.animation = `fadeIn 0.6s ease ${index * 0.1}s forwards`;
-    
-        const label = document.createElement('div');
-        label.className = 'label';
-        label.style.color = isColorDark(flavor.color) ? 'rgba(255, 255, 255, 0.75)' : 'rgba(0, 0, 0, 0.8)';
-    
+
+        const label = createElementWithClass('label');
+        label.style.color = textColor;
+
         const nameLabel = document.createElement('span');
         nameLabel.textContent = humanize(flavor.name);
         label.appendChild(nameLabel);
-    
+
+        const description = createElementWithClass('description');
+        description.style.color = textColor;
+        description.textContent = flavor.description;
+
+        element.appendChild(label);
+        element.appendChild(description);
+
         if (flavor.children?.length > 0) {
-            const countLabel = document.createElement('span');
-            countLabel.className = 'count-label';
+            const countLabel = createElementWithClass('count-label');
             countLabel.textContent = `(${flavor.children.length})`;
             label.appendChild(countLabel);
-    
-            element.classList.add('expandable');
-            const childrenContainer = document.createElement('div');
-            childrenContainer.className = 'flavor-container';
-            flavor.children.forEach((childFlavor, childIndex) => 
-                childrenContainer.appendChild(createFlavorElement(childFlavor, childIndex)));
+
+            const childrenContainer = createFlavorContainer('flavor-container');
+            flavor.children.forEach((childFlavor, childIndex) => {
+                childrenContainer.appendChild(createFlavorElement(childFlavor, childIndex, level + 1));
+            });
             element.appendChild(childrenContainer);
         }
-    
-        element.appendChild(label);
+
         element.addEventListener('click', handleFlavorClick);
-    
+
         return element;
-    };    
+    }
+
+    function createFlavorContainer(className) {
+        const container = createElementWithClass(className);
+        return container;
+    }
+
+    function createElementWithClass(className) {
+        const element = document.createElement('div');
+        element.className = className;
+        return element;
+    }
 
     try {
         const response = await fetch('coffee-flavors.json');
         const data = await response.json();
-        data.CoffeeFlavors.forEach((flavor, index) => 
-            rootContainer.appendChild(createFlavorElement(flavor, index)));
+        data.CoffeeFlavors.forEach((flavor, index) => {
+            rootContainer.appendChild(createFlavorElement(flavor, index));
+        });
     } catch (error) {
         console.error('Error loading coffee flavors:', error);
     }
