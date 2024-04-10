6dcf09129dab1ec6e644e09bbfa88e36e144ccd7
Dara Kilicoglu <darakilicoglu@gmail.com>
2024-04-10 13:24:36 +0200

    Remove loader

--- assets/app.js
+++ assets/app.js
@@ -1,18 +1,13 @@
-window.addEventListener('load', function() {
-    const loaderWrapper = document.getElementById("loader-wrapper");
-    loaderWrapper.style.display = "none";
+const scrollToTop = () => {
+    const c = document.documentElement.scrollTop || document.body.scrollTop;
+    if (c > 0) {
+        window.requestAnimationFrame(scrollToTop);
+        window.scrollTo(0, c - c / 15);
+    }
+};
 
-    const scrollToTop = () => {
-        const c = document.documentElement.scrollTop || document.body.scrollTop;
-        if (c > 0) {
-            window.requestAnimationFrame(scrollToTop);
-            window.scrollTo(0, c - c / 15);
-        }
-    };
-
-    const backToTop = document.getElementById('back-to-top');
-    backToTop.addEventListener('click', (e) => {
-        e.preventDefault();
-        scrollToTop();
-    });
+const backToTop = document.getElementById('back-to-top');
+backToTop.addEventListener('click', (e) => {
+    e.preventDefault();
+    scrollToTop();
 });
