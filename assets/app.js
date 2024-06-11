83a272074224d690cbe027f417be344cf4d77821
Dara Kilicoglu <darakilicoglu@gmail.com>
2024-06-11 23:22:49 +0200

    Styling for mobile

--- assets/app.js
+++ assets/app.js
@@ -1,18 +1,3 @@
-// document.addEventListener('scroll', function () {
-//     const titles = document.querySelectorAll('.sticky-title');
-//     titles.forEach((title, index) => {
-//         const rect = title.getBoundingClientRect();
-//         if (rect.top <= 0) {
-//             title.classList.add('active');
-//             if (index > 0) {
-//                 titles[index - 1].classList.remove('active');
-//             }
-//         } else {
-//             title.classList.remove('active');
-//         }
-//     });
-// });
-
 const scrollToTop = () => {
     const c = document.documentElement.scrollTop || document.body.scrollTop;
     if (c > 0) {
