493d4fe21b4d46305aa2e545143d36544749f00e
Dara Kilicoglu <darakilicoglu@gmail.com>
2024-06-13 23:32:47 +0200

    Update

--- assets/app.js
+++ assets/app.js
@@ -1,3 +1,27 @@
+document.addEventListener('DOMContentLoaded', () => {
+    const clickables = document.querySelectorAll('article.post[data-information] .bento');
+    const overlay = document.getElementById('overlay');
+    const overlayContent = document.getElementById('overlayContent');
+
+    clickables.forEach(clickable => {
+        clickable.addEventListener('click', (event) => {
+            const projectInfo = event.currentTarget.parentElement.getAttribute('data-information');
+            overlayContent.innerHTML = projectInfo;
+            overlay.classList.add('active');
+            document.body.style.overflow = 'hidden';
+        });
+    });
+
+    overlay.addEventListener('click', () => {
+        overlay.classList.remove('active');
+        document.body.style.overflow = '';
+    });
+
+    overlayContent.addEventListener('click', (event) => {
+        event.stopPropagation();
+    });
+});
+
 const scrollToTop = () => {
     const c = document.documentElement.scrollTop || document.body.scrollTop;
     if (c > 0) {
